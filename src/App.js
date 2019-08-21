6cf785967953c3117e578d531711b9b9f25d275b
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-08-20 19:09:01 -0600

    imported useState hook, used state hook and changed hard coded values with setState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+//TODO: done :)
+import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks. You'll need one for the home score and another for the away score.
+  //TODO: done :)
+  const [homeScore, setHomeScore] = useState(50);
+  const [awayScore, setAwayScore] = useState(32);
 
   return (
     <div className="container">
@@ -13,21 +17,21 @@ function App() {
           <div className="home">
             <h2 className="home__name">Lions</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">32</div>
+          // TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state.
+          // TODO: done :)
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
       </section>
       <section className="buttons">
         <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+          // TODO STEP 4 - Now we need to attach our state setter functions to click listeners.
           <button className="homeButtons__touchdown">Home Touchdown</button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
