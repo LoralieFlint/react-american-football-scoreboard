f13d8916f1c6720a15c98ef6c395cb442af5cc7b
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-08-20 21:27:08 -0600

    added commments cleaned code and met MVP

--- src/App.js
+++ src/App.js
@@ -36,14 +36,13 @@ function App() {
         <div className="topRow">
           <div className="home">
             <h2 className="home__name">Lions</h2>
-
             {
               // TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state.
               // TODO: done :)
             }
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">15:05</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
@@ -51,6 +50,7 @@ function App() {
         </div>
         <BottomRow />
       </section>
+
       <section className="buttons">
         <div className="homeButtons">
           {
